787a0eec697b4af4501dd94a5f2927ab18cdddd6
Mohd Tasleem Khan <mohdtasleemwork@gmail.com>
2026-05-15 02:47:31 +0530

    First Programm

--- Day_1/First_Programm.py
+++ Day_1/First_Programm.py
@@ -1,1 +1,13 @@
+# What is Programming?
+# Programming means giving instructions to a computer.
+
+# What is a Program?
+# A set of instructions written for a computer is called a program.
+    
+# What is a Programming Language?
+# A language used to communicate with computers is called a programming language.
+
+# What is Python?
+# Python is a high-level programming language created by Guido van Rossum.
+
 print("Welcome to Python")
