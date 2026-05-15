b015b0f9e777ed11aafbdcd776fd9cef0e36e4c7
Mohd Tasleem Khan <mohdtasleemwork@gmail.com>
2026-05-16 01:40:58 +0530

    Day 2 Completed

--- Day_2/string.py
+++ Day_2/string.py
@@ -1,2 +1,19 @@
+# What is a Variable?
+# A variable is a container used to store data.
+# Variable Naming Rules
+# 1. Variable cannot start with a number.
+# 2. Spaces are not allowed in variable names.
+# 3. Special characters are not allowed.
+
+# Creating a Variable :
+
 name = "Mohd Tasleem Khan"
+age = 27
+Home_Address = "Uttar Pradesh"
+print(name, age, Home_Address)
+
+print() # for one line space
+
 print(name)
+print(age)
+print(Home_Address)
