5547e86ab3e340d365f73bab8e5cae3c0b136c93
Mohd Tasleem Khan <mohdtasleemwork@gmail.com>
2026-05-21 01:42:12 +0530

    add new file

---
+++ Number_Operations_Practice_Set/question_4.py
@@ -0,0 +1,8 @@
+# Multiply two Numbers :
+
+num1 = int(input("Enter first Number : "))
+num2 = int(input("Enter first Number : "))
+
+multiply = num1 * num2
+
+print("The Multiplication is : ", multiply)
